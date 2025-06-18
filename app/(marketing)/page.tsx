8a6d885e1b10cb990a30a34fd4c03346e1a157aa
adavidryu <adavidryu@gmail.com>
2025-06-18 11:37:02 -0500

    fix: theme and layout bugs

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,26 +3,14 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <Navbar />
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center mt-20">
+      <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <Hero />
         </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
       </footer>
     </div>
   );

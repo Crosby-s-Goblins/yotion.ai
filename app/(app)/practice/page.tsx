a6aae1c358a126d2f8665af46c0f60b1447a3b5d
Romeo Beaurain <beaurainr@Romeos-MacBook-Air-2.local>
2025-06-24 13:38:17 -0500

    fully responsive menu design

--- app/(app)/practice/page.tsx
+++ app/(app)/practice/page.tsx
@@ -3,78 +3,74 @@ import { Settings, Users, ChartNoAxesCombined, Flame } from 'lucide-react';
 
 export default function PracticePage() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen -mt-12 gap-24">
+    <main className="flex flex-col items-center justify-center w-full 2xl:h-screen 2xl:items-center">
         <div>
-            <h1 className="text-3xl font-semibold">yotion.ai</h1>
+            <h1 className="text-3xl font-semibold my-10 my-5">yotion.ai</h1>
         </div>
-        <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col space-y-6">
-                <div className="flex flex-row gap-8 justify-center">
-                    <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
-                    <a href="/selection" className="w-full h-full flex flex-col justify-center items-center">
-                        <CardHeader className="flex flex-col justify-center items-center">
-                            <CardTitle className="text-2xl">Train</CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col justify-center items-center">
-                            <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
-                                <Flame className="w-32 h-32"/>
-                            </div>
-                            <p className="text-sm text-muted-foreground text-center">
-                                Choose from our curated list of poses and get real-time feedback on your form.
-                            </p>
-                        </CardContent>
-                    </a>
-                    </Card>
+        <div className="flex flex-col gap-5 md:grid md:grid-cols-2 2xl:grid-cols-4">
+            <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
+            <a href="/selection" className="w-full h-full flex flex-col justify-center items-center">
+                <CardHeader className="flex flex-col justify-center items-center">
+                    <CardTitle className="text-2xl">Train</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-center items-center">
+                    <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
+                        <Flame className="w-32 h-32"/>
+                    </div>
+                    <p className="text-sm text-muted-foreground text-center">
+                        Choose from our curated list of poses and get real-time feedback on your form.
+                    </p>
+                </CardContent>
+            </a>
+            </Card>
 
-                    <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
-                    <a href="/performance" className="w-full h-full flex flex-col justify-center items-center">
-                        <CardHeader className="flex flex-col justify-center items-center">
-                            <CardTitle className="text-2xl">Performance</CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col justify-center items-center">
-                            <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
-                                <ChartNoAxesCombined className="w-32 h-32"/>
-                            </div>
-                            <p className="text-sm text-muted-foreground text-center">
-                                Track your progress and see improvements over time.
-                            </p>
-                        </CardContent>
-                    </a>
-                    </Card>
+            <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
+            <a href="/performance" className="w-full h-full flex flex-col justify-center items-center">
+                <CardHeader className="flex flex-col justify-center items-center">
+                    <CardTitle className="text-2xl">Performance</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-center items-center">
+                    <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
+                        <ChartNoAxesCombined className="w-32 h-32"/>
+                    </div>
+                    <p className="text-sm text-muted-foreground text-center">
+                        Track your progress and see improvements over time.
+                    </p>
+                </CardContent>
+            </a>
+            </Card>
 
-                    <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
-                    <a href="/community" className="w-full h-full flex flex-col justify-center items-center">
-                        <CardHeader className="flex flex-col justify-center items-center">
-                            <CardTitle className="text-2xl">Community</CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col justify-center items-center">
-                            <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
-                                <Users className="w-32 h-32"/>
-                            </div>
-                            <p className="text-sm text-muted-foreground text-center">
-                                Connect with other yoga practitioners and share experiences.
-                            </p>
-                        </CardContent>
-                    </a>
-                    </Card>
+            <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
+            <a href="/community" className="w-full h-full flex flex-col justify-center items-center">
+                <CardHeader className="flex flex-col justify-center items-center">
+                    <CardTitle className="text-2xl">Community</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-center items-center">
+                    <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
+                        <Users className="w-32 h-32"/>
+                    </div>
+                    <p className="text-sm text-muted-foreground text-center">
+                        Connect with other yoga practitioners and share experiences.
+                    </p>
+                </CardContent>
+            </a>
+            </Card>
 
-                    <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
-                    <a href="/appSettings" className="w-full h-full flex flex-col justify-center items-center">
-                        <CardHeader className="flex flex-col justify-center items-center">
-                            <CardTitle className="text-2xl">Settings</CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col justify-center items-center">
-                            <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
-                                <Settings className="w-32 h-32"/>
-                            </div>
-                            <p className="text-sm text-muted-foreground text-center">
-                                Customize your experience and manage your preferences.
-                            </p>
-                        </CardContent>
-                    </a>
-                    </Card>
-                </div>
-            </div>
+            <Card className="flex flex-col justify-center items-center w-80 h-80 aspect-square transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-200/50 cursor-pointer">
+            <a href="/appSettings" className="w-full h-full flex flex-col justify-center items-center">
+                <CardHeader className="flex flex-col justify-center items-center">
+                    <CardTitle className="text-2xl">Settings</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-center items-center">
+                    <div className="w-32 h-32 rounded-lg mb-4 flex items-center justify-center">
+                        <Settings className="w-32 h-32"/>
+                    </div>
+                    <p className="text-sm text-muted-foreground text-center">
+                        Customize your experience and manage your preferences.
+                    </p>
+                </CardContent>
+            </a>
+            </Card>
         </div>
     </main>
   );
